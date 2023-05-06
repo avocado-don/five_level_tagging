9cc66210b6c5866f1bfe3fb760d10f521e4ebfb7
avocado-don <hatonomuneniku79@yahoo.co.jp>
2023-05-06 23:58:29 +0900

    #5-2 リスト：詳細・編集・削除
    詳細画面：選択したリストの詳細を表示。
    編集画面：自身が投稿したリストを編集・更新。
    削除機能(詳細画面)：自身が投稿したリストを削除。

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
 
   def index
     @q = List.ransack(params[:q])
@@ -13,14 +14,38 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to root_path
+      redirect_to list_path(@list.id)
     else
       render :new
     end
   end
 
+  def show
+  end
+
+  def edit
+    redirect_to root_path unless current_user.id == @list.user_id
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to list_path(@list.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list.destroy if current_user.id == @list.user_id
+    redirect_to root_path
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :concept, :score_rule).merge(user_id: current_user.id)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def safe_linebreak(str)
+    h(str).gsub(/\n|\r|\r\n/, '<br>').html_safe
+  end
 end
